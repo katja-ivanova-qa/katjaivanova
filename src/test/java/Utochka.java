b81c48c9d698cdfd7e148db6bbe6cdeb68fea653
katja <jekaterina.rudzite@gmail.com>
2020-02-09 13:41:39 +0200

    hi

---
+++ src/test/java/Utochka.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+public class Utochka {
+    @Test
+
+    public void utochka() {
+        int k = 100;
+        int i = 2;
+        int o = k - i;
+
+        System.out.println(o);
+
+
+        String g = "100";
+        String z = "2";
+        String p = g + z;
+
+        System.out.println(p);
+    }
+
+}
