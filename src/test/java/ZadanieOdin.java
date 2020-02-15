ec8e3579ae2e91d388ebd37ca859d025c9387110
katja <jekaterina.rudzite@gmail.com>
2020-02-15 22:34:12 +0200

    hi

---
+++ src/test/java/ZadanieOdin.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class ZadanieOdin {
+    @Test
+    public void zadanieOdin() {
+        String marka = "Жига";
+        String probeg = "999999";
+        String cena = "00.01";
+
+        System.out.println("Продаем машину " + marka + " с пробегом " + probeg + " за " + cena + " денег");
+    }
+}
